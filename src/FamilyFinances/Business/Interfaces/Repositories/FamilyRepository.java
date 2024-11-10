ce8b056cb5b47962bc88d9940971f4d6a44c0d70
John Carlos Arrieta Arrieta <johncarlosarrietaarrieta@gmail.com>
2024-11-10 08:06:19 -0500

    Interface que declara los métodos para realizar persistencia de entidades de tipo Familia.

---
+++ src/FamilyFinances/Business/Interfaces/Repositories/FamilyRepository.java
@@ -0,0 +1,24 @@
+package FamilyFinances.Business.Interfaces.Repositories;
+
+import FamilyFinances.Business.Exceptions.DuplicateFamilyEntityException;
+import FamilyFinances.Business.Exceptions.FamilyEntityNotFoundException;
+import FamilyFinances.Domain.Models.Family;
+import java.util.List;
+
+/**
+ *
+ * @author johnarrieta
+ */
+public interface FamilyRepository {
+   public Family findById(Integer id) throws FamilyEntityNotFoundException, Exception;
+    
+    public Family findByCode(String code) throws FamilyEntityNotFoundException, Exception;
+
+    public List<Family> getAll() throws Exception;
+
+    public void save(Family role) throws DuplicateFamilyEntityException, Exception;
+
+    public void edit(Family role) throws FamilyEntityNotFoundException, Exception;
+
+    public void deleteById(Integer id) throws FamilyEntityNotFoundException, Exception;
+}
